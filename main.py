f9419dc58b997d52608ce58a0405ceaa961064ca
Matthew Bowers <matbowrs@gmail.com>
2024-12-14 22:43:22 -0500

    create player cobject, can rotate in a circle... woohoo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -7,14 +8,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_clock = pygame.time.Clock() 
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        player_1.draw(screen)
+        player_1.update(dt)
         pygame.display.flip()
 
+        # Divide by 1000 t convert from milliseconds to seconds
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
