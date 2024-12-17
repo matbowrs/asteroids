28aa2ef18106b35bfe7298a1606b3029b564a02a
Matthew Bowers <matbowrs@gmail.com>
2024-12-16 22:37:29 -0500

    add Shot class and method to o sho shoot bullets from player's position

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from constants import *
 from player import Player
+from shot import Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
@@ -14,10 +15,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Shot.containers = (shots, drawable, updatable)
 
     Asteroid.containers = (asteroids, updatable, drawable)
 
